412952ffcc45046d7588b1e5b04bb6c94f22cf0d
luigi.abenante <luigi.abenante@soft.it>
2024-12-20 22:58:03 +0100

    ### keymaps  - added neogit keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,5 @@
 ----------------------------------------------------------------------------
--- KEYMAPS 
+-- KEYMAPS
 
 vim.g.mapleader = " "
 
@@ -22,8 +22,14 @@ vim.keymap.set("n", "<leader>ef", "<CMD>Oil<CR>", { desc = "Open parent director
 vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
 
 --NVIM-TREE
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file explorer" }) -- focus file explorer
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>et", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+keymap.set("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file explorer" }) -- focus file explorer
+keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+keymap.set("n", "<leader>et", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+
+-- NEOGIT
+keymap.set("n", "<leader>go", "<cmd>Neogit<CR>", { desc = "Neogit Open" })
+keymap.set("n", "<leader>gc", "<cmd>Neogit commit<CR>", { desc = "Neogit commit" })
+keymap.set("n", "<leader>gp", "<cmd>Neogit push<CR>", { desc = "Neogit push" })
+keymap.set("n", "<leader>gl", "<cmd>Neogit pull<CR>", { desc = "Neogit pull" })
